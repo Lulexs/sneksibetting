d60a13ef21910bf96879dbced0f5a9b3fafa1f36
Luka <luleee@elfak.rs>
2025-04-12 19:04:00 +0200

    first commit

---
+++ src/main/java/com/luka/sneksibetting/SneksibettingApplication.java
@@ -0,0 +1,13 @@
+package com.luka.sneksibetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneksibettingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SneksibettingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luka/sneksibetting/SneksibettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luka.sneksibetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneksibettingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
