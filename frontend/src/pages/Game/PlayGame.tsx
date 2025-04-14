128bee3bc42ba8bbb452d3fceff2be220528cd4c
Luka <luka.velickovic@dualsoft.net>
2025-04-14 14:55:30 +0200

    Moved initial handshake to appropriate component

--- frontend/src/pages/Game/PlayGame.tsx
+++ frontend/src/pages/Game/PlayGame.tsx
@@ -1,8 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useStore } from "../../stores/store";
 import { useNavigate } from "react-router";
 import { Flex } from "@mantine/core";
 import Board, { BoardsProps } from "./Boards";
+import { TypedMessage, QueuedMessage } from "../../models/Message";
+import useWebSocket from "react-use-websocket";
 
 const booleanMatrix: number[][] = Array.from({ length: 10 }, () =>
   Array(10).fill(0)
@@ -29,9 +31,70 @@ const testObj: BoardsProps = {
   num_watching: 4,
 };
 
+function getBytes(type: number, message: any) {
+  const json = JSON.stringify(message);
+  const jsonBytes = new TextEncoder().encode(json);
+
+  const buffer = new ArrayBuffer(4 + jsonBytes.length);
+  const view = new DataView(buffer);
+  view.setInt32(0, type);
+  new Uint8Array(buffer, 4).set(jsonBytes);
+
+  return buffer;
+}
+
+async function getMessage(buf: Blob): Promise<TypedMessage> {
+  if (buf == null || buf == undefined) {
+    return { id: 0, message: { id: 0 } };
+  }
+  console.log(typeof buf);
+  console.log(buf);
+  const dv = new DataView(await buf.arrayBuffer());
+
+  const type = dv.getInt32(0);
+
+  switch (type) {
+    case 2:
+      return { id: type, message: new QueuedMessage(type) };
+
+    default:
+      return { id: 0, message: { id: 0 } };
+  }
+}
+
 export default function PlayGame() {
   const { userStore } = useStore();
   const navigate = useNavigate();
+
+  const socketUrl = "ws://localhost:8080/ws";
+
+  const { sendMessage, lastMessage } = useWebSocket(socketUrl);
+  const [state, setState] = useState(0);
+
+  useEffect(() => {
+    if (userStore.user == null) {
+      navigate("/");
+    }
+  }, [userStore.user]);
+
+  useEffect(() => {
+    if (state == 0) {
+      sendMessage(getBytes(1, { userId: userStore.user?.uuid }));
+    }
+  }, []);
+
+  useEffect(() => {
+    const processMessage = async () => {
+      if (lastMessage && lastMessage.data instanceof Blob) {
+        const parsedMessage = await getMessage(lastMessage.data);
+        if (parsedMessage.id == 2) {
+          setState(2);
+        }
+      }
+    };
+
+    processMessage();
+  }, [lastMessage]);
 
   useEffect(() => {
     if (userStore.user == null) {
@@ -41,7 +104,9 @@ export default function PlayGame() {
 
   return (
     <Flex m="lg" h="85vh" align="center" justify="center">
-      <Board {...testObj} />
+      {state == 0 && <h1>Waiting for server</h1>}
+      {state == 2 && <h1>Waiting for opponent</h1>}
+      {state == 4 && <Board {...testObj} />}
     </Flex>
   );
 }
--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,57 +1,11 @@
 import { Button, Flex, Group } from "@mantine/core";
-import { useEffect } from "react";
 import { useNavigate } from "react-router";
-import useWebSocket from "react-use-websocket";
-import { QueuedMessage, TypedMessage } from "../../models/Message";
 
-function getBytes(type: number, message: any) {
-  const json = JSON.stringify(message);
-  const jsonBytes = new TextEncoder().encode(json);
 
-  const buffer = new ArrayBuffer(4 + jsonBytes.length);
-  const view = new DataView(buffer);
-  view.setInt32(0, type);
-  new Uint8Array(buffer, 4).set(jsonBytes);
-
-  return buffer;
-}
-
-async function getMessage(buf: Blob) : Promise<TypedMessage> {
-  if (buf == null || buf == undefined) {
-    return {id: 0, message: {id: 0}}
-  }
-  console.log(typeof buf)
-  console.log(buf)
-  const dv = new DataView(await buf.arrayBuffer());
-
-  const type = dv.getInt32(0);
-
-  switch (type) {
-    case 2:
-      return {id: type, message: new QueuedMessage(type)} 
-  
-    default:
-      return {id: 0, message: {id: 0}};
-  }
-}
 
 export default function HomePage() {
   const navigate = useNavigate();
 
-  const socketUrl = "ws://localhost:8080/ws";
-
-  const {sendMessage, lastMessage} = useWebSocket(socketUrl);
-
-
-  useEffect(() => {
-    const processMessage = async () => {
-      if (lastMessage && lastMessage.data instanceof Blob) {
-        const parsedMessage = await getMessage(lastMessage.data);
-      }
-    };
-  
-    processMessage();
-  }, [lastMessage]);
 
 
   return (
@@ -61,7 +15,6 @@ export default function HomePage() {
         <Button onClick={() => navigate("/register")}>Register</Button>
       </Group>
       <h1>WELCOME TO SNEKSI BETTING</h1>
-      <Button onClick={() => {sendMessage(getBytes(1, {userId: "5516e359-6c9c-4ebb-a409-52373d536d50"}))}}>Clickme</Button>
     </Flex>
   );
 }
